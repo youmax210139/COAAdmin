3a344afc8403124dd7d47e0c1f8e2092ac25c06f
charlieYuan <youmax210139@gmail.com>
2019-09-15 12:20:52 +0800

    feat: common header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -61,65 +61,7 @@
         </a>
     </div>
     <!--上版-->
-    <header id="header">
-        <div class="inner">
-            <div id="header_logo">
-                <a href="{{ route('homes.index') }}">
-                    <img src="{{ asset("images/logo_".app()->getLocale().".svg") }}" alt="{{ trans('custom.page_title') }}">
-                </a>
-            </div>
-            <nav id="header_nav">
-                <div id="m_nav">
-                    <div id="menu">
-                        <ul class="menu">
-                            <li>
-                                <a href="#about" title="{{ trans('custom.traceabilty_about') }}" data-scroll="#about">
-                                    {{ trans('custom.traceabilty_about') }}
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#core" title="{{ trans('custom.blockchain_about') }}" data-scroll="#core">
-                                    {{ trans('custom.blockchain_about') }}
-                                </a>
-                            </li>
-                            <li><a href="{{ route('resumes.inquiry') }}" title="{{ trans('custom.resume_search') }}">{{ trans('custom.resume_search') }}</a> </li>
-                            <li><a href="{{ route('resumes.index') }}" title="{{ trans('custom.resume_latest') }}">{{ trans('custom.resume_latest') }}</a></li>
-                            @if(config('setting.neo4j'))
-                            <li>
-                                <a href="{{ route('neo4j.view','441748') }}" title="neo4j">
-                                    Neo4j
-                                </a>
-                            </li>
-                            @endif
-                        </ul>
-                        <label class="switch">
-                            <input type="checkbox" id="togBtn" {{ session('locale')=='en'?'checked':'' }}>
-                            <div class="slider round"></div>
-                        </label>
-                    </div>
-                    <a href="javascript:$.pageslide.close()" class="bars_close"></a>
-                </div>
-            </nav>
-            <div id="banner">
-                <div class="img"><img src="images/banner_img.svg"></div>
-                <div class="info">
-                    <div class="txt">
-                        <h2>{{ trans('custom.slogan_1') }}</h2>
-                        <p>{{ trans('custom.slogan_2') }}</p>
-                        <p>{{ trans('custom.slogan_3') }}</p>
-                        <p>{{ trans('custom.slogan_4') }}</p>
-                    </div>
-                    <div class="search_box">
-                        <form action="{{ route('resumes.index') }}" method="get" autocomplete="off">
-                            <input name="farm" type="text" class="srch_input" placeholder="{{ trans('custom.please_enter') }}{{ trans('custom.farm') }}">
-                            <input name="product" type="text" class="srch_input" placeholder="{{ trans('custom.please_enter') }}{{ trans('custom.crop_code') }}">
-                            <button type="submit" class="ico_enter"><span class="blind">{{ trans('custom.search') }}</span></button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
+    @include('partials.header')
     <main id="main">
         <div class="inner">
             <section id="about">
--- resources/views/resumes/inquiry.blade.php
+++ resources/views/resumes/inquiry.blade.php
@@ -22,47 +22,7 @@
             <div class="ico"><span></span></div>
         </a> </div>
     <!--上版-->
-    <header id="header">
-        <div class="inner">
-            <div id="header_logo">
-                <a href="{{route('homes.index')}}">
-                    <img src="{{ asset("images/logo_".app()->getLocale().".svg") }}" alt="{{ trans('custom.page_title') }}">
-                </a>
-            </div>
-            <nav id="header_nav">
-                <div id="m_nav">
-                    <div id="menu">
-                        <ul class="menu">
-                            <li>
-                                <a href="{{ route('homes.index','#about') }}" title="{{ trans('custom.traceabilty_about') }}" data-scroll="#about">
-                                    {{ trans('custom.traceabilty_about') }}
-                                </a>
-                            </li>
-                            <li>
-                                <a href="{{ route('homes.index','#core') }}" title="{{ trans('custom.blockchain_about') }}" data-scroll="#core">
-                                    {{ trans('custom.blockchain_about') }}
-                                </a>
-                            </li>
-                            <li class="active"><a href="{{ route('resumes.inquiry') }}" title="{{ trans('custom.resume_search') }}">{{ trans('custom.resume_search') }}</a> </li>
-                            <li><a href="{{ route('resumes.index') }}" title="{{ trans('custom.resume_latest') }}">{{ trans('custom.resume_latest') }}</a></li>
-                            @if(config('setting.neo4j'))
-                            <li>
-                                <a href="{{ route('neo4j.view','441748') }}" title="neo4j">
-                                    Neo4j
-                                </a>
-                            </li>
-                            @endif
-                        </ul>
-                        <label class="switch">
-                            <input type="checkbox" id="togBtn" {{ session('locale')=='en'?'checked':'' }}>
-                            <div class="slider round"></div>
-                        </label>
-                    </div>
-                    <a href="javascript:$.pageslide.close()" class="bars_close"></a>
-                </div>
-            </nav>
-        </div>
-    </header>
+    @include('partials.header')
     <main id="main">
         <div class="inner">
             <section id="rsu_inquire">
